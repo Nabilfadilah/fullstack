72fd2b9373e1c423d282b0b5aaeca1e189fcd55e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-11 16:29:39 +0700

    Menghapus property connect mongoodb

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,24 +3,21 @@ import mongoose from "mongoose";
 let isConnected = false; // track connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true);
+  mongoose.set('strictQuery', true);
 
-    if(isConnected) {
-        console.log("MongoDB is already connection");
-        return;
-    }
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+    });
 
-        isConnected = true;
-
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.log(error);
-    }
-}
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log("MongoDB connection error:", error);
+  }
+};
