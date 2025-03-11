014b5b8d85c4fdf3b8165d643d976db94b1b613a
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-11 16:27:25 +0700

    Memberikan configurasi untuk image dari user yang login

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverExternalPackages: ["mongoose"], // ✅ pindahkan ke luar experimental
+
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+    },
+
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    }
+};
 
 export default nextConfig;
