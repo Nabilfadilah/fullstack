ce5c7691784d40d7eb9c9e8ae4e2c9824079fc2c
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-14 16:28:56 +0700

    Membuat page profile dan menjalankan fungsi fetch api get

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Profile from "@/components/Profile";
+import {useSession} from "next-auth/react";
+import React, {useEffect, useState} from "react";
+
+const page = () => {
+  const {data: session} = useSession();
+  const [posts, setPosts] = useState([]);
+
+  // Mengambil data prompt dari API ketika komponen pertama kali dirender
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`); // Memanggil endpoint API untuk mendapatkan data prompt
+      const data = await response.json(); // Mengonversi response menjadi format JSON
+
+      setPosts(data); // Menyimpan data prompt ke dalam state
+    };
+
+    if (session?.user.id) fetchPost();
+  }, []); // Efek ini hanya dijalankan sekali saat komponen pertama kali dimuat (karena dependensi [] kosong)
+
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
+};
+
+export default page;
